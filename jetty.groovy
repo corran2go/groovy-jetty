26ce66ce3131a247ec4751e28164b10e8212e0a6
Steffen Werner <corran2go@gmail.com>
2011-12-07 09:21:44 +0100

    base version with GSP support

---
+++ jetty.groovy
@@ -0,0 +1,14 @@
+import org.mortbay.jetty.Server
+import org.mortbay.jetty.servlet.*
+import groovy.servlet.*
+
+@Grab(group = 'org.mortbay.jetty', module = 'jetty-embedded', version = '6.1.0')
+def runServer() {
+  def server = new Server(8080)
+  def context = new Context(server, "/", Context.SESSIONS);
+  context.resourceBase = "."
+  context.addServlet(TemplateServlet, "*.gsp")
+  server.start()
+}
+
+runServer()
